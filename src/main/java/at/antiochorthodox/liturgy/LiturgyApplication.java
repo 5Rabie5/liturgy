172bd90ee30bfe54debc9e1e0ff78c2d4b423b88
5Rabie5 <n.rabietarboush@gmail.com>
2025-06-16 23:03:43 +0200

    init

---
+++ src/main/java/at/antiochorthodox/liturgy/LiturgyApplication.java
@@ -0,0 +1,13 @@
+package at.antiochorthodox.liturgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiturgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiturgyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/antiochorthodox/liturgy/LiturgyApplicationTests.java
@@ -0,0 +1,13 @@
+package at.antiochorthodox.liturgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiturgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
